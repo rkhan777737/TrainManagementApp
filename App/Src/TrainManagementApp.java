fb28cd4e511ff1147209ca64cbd47f4f4f49ed64
Mohammed Rehan Khan <rkhan777737@gmail.com>
2026-04-01 14:27:40 +0530

    TrainManagementApp

--- App/Src/TrainManagementApp.java
+++ App/Src/TrainManagementApp.java
@@ -1,10 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * TrainManagementApp.java
- * UC1: Initialize Train and Display Consist Summary
- */
 public class TrainManagementApp {
 
     public static void main(String[] args) {
@@ -14,21 +7,5 @@ public class TrainManagementApp {
         System.out.println("==========================================");
         System.out.println();
 
-        // 2. Dynamic Collection Initialization
-        // We initialize an empty List to hold our train's consist.
-        // The ArrayList allows the consist to grow as bogies are added later.
-        List<String> trainConsist = new ArrayList<>();
-
-        // 3. Display Initial State
-        System.out.println("Train initialized successfully...");
-
-        // size() returns 0 since no bogies have been added yet
-        System.out.println("Initial Bogie Count : " + trainConsist.size());
-
-        // Printing the list object directly shows the empty brackets []
-        System.out.println("Current Train Consist : " + trainConsist);
-
-        System.out.println();
-        System.out.println("System ready for operations...");
     }
 }
